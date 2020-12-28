86db4ed92ce4dbd5b539a3ae385d406f9ea588a9
Bastian Burger <dev@minpik.com>
2020-12-28 14:47:54 +0100

    Added asynchronous Batch with parameter validation

---
+++ MoreLinq/Experimental/Async/Batch.cs
@@ -0,0 +1,67 @@
+﻿#region License and Terms
+// MoreLINQ - Extensions to LINQ to Objects
+// Copyright (c) 2020 Atif Aziz. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+#if !NO_ASYNC_STREAMS
+
+namespace MoreLinq.Experimental.Async
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    static partial class MoreAsyncEnumerable
+    {
+        /// <summary>
+        /// Batches the source sequence into sized buckets.
+        /// </summary>
+        /// <param name="source">The source sequence.</param>
+        /// <param name="size">Size of buckets.</param>
+        /// <typeparam name="TSource">Type of elements in <paramref name="source"/> sequence.</typeparam>
+        /// <returns>An asynchronous sequence of equally sized buckets containing elements of the source collection.</returns>
+        public static IAsyncEnumerable<IEnumerable<TSource>> Batch<TSource>(this IAsyncEnumerable<TSource> source, int size) =>
+            Batch(source, size, x => x);
+
+        /// <summary>
+        /// Batches the source sequence into sized buckets and applies a projection to each bucket.
+        /// </summary>
+        /// <param name="source">The source sequence.</param>
+        /// <param name="size">Size of buckets.</param>
+        /// <param name="resultSelector"></param>
+        /// <typeparam name="TSource">Type of elements in <paramref name="source"/> sequence.</typeparam>
+        /// <typeparam name="TResult">Type of result returned by <paramref name="resultSelector"/>.</typeparam>
+        /// <returns>An asynchronous sequence of equally sized buckets containing elements of the source collection.</returns>
+        public static IAsyncEnumerable<IEnumerable<TSource>> Batch<TSource, TResult>(this IAsyncEnumerable<TSource> source, int size, Func<IEnumerable<TSource>, TResult> resultSelector)
+        {
+            if (source == null) throw new ArgumentNullException(nameof(source));
+            if (size <= 0) throw new ArgumentOutOfRangeException(nameof(size));
+            if (resultSelector == null) throw new ArgumentNullException(nameof(resultSelector));
+
+            return BatchInternal(source, size, resultSelector);
+        }
+
+        private static async IAsyncEnumerable<IEnumerable<TSource>> BatchInternal<TSource, TResult>(
+            this IAsyncEnumerable<TSource> source, int size, Func<IEnumerable<TSource>, TResult> resultSelector)
+        {
+            // TODO: implement
+            await Task.CompletedTask;
+            yield return Enumerable.Empty<TSource>();
+        }
+    }
+}
+
+#endif // !NO_ASYNC_STREAMS
---
+++ MoreLinq/Experimental/Async/MoreAsyncEnumerable.cs
@@ -0,0 +1,32 @@
+﻿#region License and Terms
+// MoreLINQ - Extensions to LINQ to Objects
+// Copyright (c) 2020 Atif Aziz. All rights reserved.
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+#if !NO_ASYNC_STREAMS
+
+namespace MoreLinq.Experimental.Async
+{
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// Provides a set of static methods for querying objects that
+    /// implement <see cref="IAsyncEnumerable{T}" />.
+    /// </summary>
+    public static partial class MoreAsyncEnumerable
+    {
+    }
+}
+
+#endif // !NO_ASYNC_STREAMS
