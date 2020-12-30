7a32e59540878d72a2ec26617a0fed391d510a9d
Bastian Burger <dev@minpik.com>
2020-12-30 10:32:21 +0100

    Add async sequence Batch operator

--- MoreLinq.Test/Async/BatchTest.cs
+++ MoreLinq.Test/Async/BatchTest.cs
@@ -1,4 +1,5 @@
 ﻿#region License and Terms
+
 // MoreLINQ - Extensions to LINQ to Objects
 // Copyright (c) 2020 Atif Aziz. All rights reserved.
 //
@@ -13,11 +14,13 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 #endregion
 
 namespace MoreLinq.Test.Async
 {
     using System.Linq;
+    using System.Threading.Tasks;
     using NUnit.Framework;
     using MoreLinq.Experimental.Async;
 
@@ -27,15 +30,58 @@ namespace MoreLinq.Test.Async
         [Test]
         public void BatchZeroSize()
         {
-            AssertThrowsArgument.OutOfRangeException("size",() =>
+            AssertThrowsArgument.OutOfRangeException("size", () =>
                 AsyncEnumerable.Empty<object>().Batch(0));
         }
 
         [Test]
         public void BatchNegativeSize()
         {
-            AssertThrowsArgument.OutOfRangeException("size",() =>
+            AssertThrowsArgument.OutOfRangeException("size", () =>
                 AsyncEnumerable.Empty<object>().Batch(-1));
+        }
+
+        [Test]
+        public async Task BatchEvenlyDistributedAsyncSequence()
+        {
+            var result = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9}.ToAsyncEnumerable().Batch(3);
+            var enumerator = result.GetAsyncEnumerator();
+            await enumerator.MoveNextAsync();
+            Assert.That(enumerator.Current, Is.EquivalentTo(new[] {1, 2, 3}));
+            await enumerator.MoveNextAsync();
+            Assert.That(enumerator.Current, Is.EquivalentTo(new[] {4, 5, 6}));
+            await enumerator.MoveNextAsync();
+            Assert.That(enumerator.Current, Is.EquivalentTo(new[] {7, 8, 9}));
+            await enumerator.DisposeAsync();
+        }
+
+        [Test]
+        public async Task BatchUnevenlyDivisibleAsyncSequence()
+        {
+            var result = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9}.ToAsyncEnumerable().Batch(4);
+            var enumerator = result.GetAsyncEnumerator();
+            await enumerator.MoveNextAsync();
+            Assert.That(enumerator.Current, Is.EquivalentTo(new[] {1, 2, 3, 4}));
+            await enumerator.MoveNextAsync();
+            Assert.That(enumerator.Current, Is.EquivalentTo(new[] {5, 6, 7, 8}));
+            await enumerator.MoveNextAsync();
+            Assert.That(enumerator.Current, Is.EquivalentTo(new[] {9}));
+            await enumerator.DisposeAsync();
+        }
+
+        [Test]
+        public async Task BatchSequenceTransformingResult()
+        {
+            var result = new[] {1, 2, 3, 4, 5, 6, 7, 8, 9}.ToAsyncEnumerable()
+                .Batch(4, batch => batch.Sum());
+            var enumerator = result.GetAsyncEnumerator();
+            await enumerator.MoveNextAsync();
+            Assert.That(enumerator.Current, Is.EqualTo(10));
+            await enumerator.MoveNextAsync();
+            Assert.That(enumerator.Current, Is.EqualTo(26));
+            await enumerator.MoveNextAsync();
+            Assert.That(enumerator.Current, Is.EqualTo(9));
+            await enumerator.DisposeAsync();
         }
     }
 }
--- MoreLinq/Experimental/Async/Batch.cs
+++ MoreLinq/Experimental/Async/Batch.cs
@@ -1,4 +1,5 @@
 ﻿#region License and Terms
+
 // MoreLINQ - Extensions to LINQ to Objects
 // Copyright (c) 2020 Atif Aziz. All rights reserved.
 //
@@ -13,6 +14,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+
 #endregion
 
 #if !NO_ASYNC_STREAMS
@@ -21,8 +23,6 @@ namespace MoreLinq.Experimental.Async
 {
     using System;
     using System.Collections.Generic;
-    using System.Linq;
-    using System.Threading.Tasks;
 
     static partial class MoreAsyncEnumerable
     {
@@ -33,7 +33,8 @@ namespace MoreLinq.Experimental.Async
         /// <param name="size">Size of buckets.</param>
         /// <typeparam name="TSource">Type of elements in <paramref name="source"/> sequence.</typeparam>
         /// <returns>An asynchronous sequence of equally sized buckets containing elements of the source collection.</returns>
-        public static IAsyncEnumerable<IEnumerable<TSource>> Batch<TSource>(this IAsyncEnumerable<TSource> source, int size) =>
+        public static IAsyncEnumerable<IEnumerable<TSource>> Batch<TSource>(
+            this IAsyncEnumerable<TSource> source, int size) =>
             Batch(source, size, x => x);
 
         /// <summary>
@@ -45,7 +46,9 @@ namespace MoreLinq.Experimental.Async
         /// <typeparam name="TSource">Type of elements in <paramref name="source"/> sequence.</typeparam>
         /// <typeparam name="TResult">Type of result returned by <paramref name="resultSelector"/>.</typeparam>
         /// <returns>An asynchronous sequence of equally sized buckets containing elements of the source collection.</returns>
-        public static IAsyncEnumerable<IEnumerable<TSource>> Batch<TSource, TResult>(this IAsyncEnumerable<TSource> source, int size, Func<IEnumerable<TSource>, TResult> resultSelector)
+        public static IAsyncEnumerable<TResult> Batch<TSource, TResult>(
+            this IAsyncEnumerable<TSource> source, int size,
+            Func<IEnumerable<TSource>, TResult> resultSelector)
         {
             if (source == null) throw new ArgumentNullException(nameof(source));
             if (size <= 0) throw new ArgumentOutOfRangeException(nameof(size));
@@ -54,12 +57,34 @@ namespace MoreLinq.Experimental.Async
             return BatchInternal(source, size, resultSelector);
         }
 
-        private static async IAsyncEnumerable<IEnumerable<TSource>> BatchInternal<TSource, TResult>(
-            this IAsyncEnumerable<TSource> source, int size, Func<IEnumerable<TSource>, TResult> resultSelector)
+        private static async IAsyncEnumerable<TResult> BatchInternal<TSource, TResult>(
+            this IAsyncEnumerable<TSource> source, int size,
+            Func<IEnumerable<TSource>, TResult> resultSelector)
         {
-            // TODO: implement
-            await Task.CompletedTask;
-            yield return Enumerable.Empty<TSource>();
+            TSource[]? bucket = null;
+            var count = 0;
+
+            await foreach (var item in source)
+            {
+                bucket ??= new TSource[size];
+                bucket[count++] = item;
+
+                // The bucket is fully buffered before it's yielded
+                if (count < size)
+                    continue;
+
+                yield return resultSelector(bucket);
+
+                bucket = null;
+                count = 0;
+            }
+
+            // Return the last bucket with all remaining elements
+            if (bucket != null)
+            {
+                Array.Resize(ref bucket, count);
+                yield return resultSelector(bucket);
+            }
         }
     }
 }
